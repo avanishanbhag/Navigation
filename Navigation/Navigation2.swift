23433d7fb76a90bb58d1fec2a94db384b4264b10
Avani Shanbhag <shanbhagavani@gmail.com>
2025-07-10 10:25:30 -0400

    Created nav2 page and nav stack as well as nav link to navigate between pages.

---
+++ Navigation/Navigation2.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation2.swift
+//  Navigation
+//
+//  Created by Scholar on 7/10/25.
+//
+
+import SwiftUI
+
+struct Navigation2: View {
+    var body: some View {
+        Text("You have arrived at the second view!")
+    }
+}
+
+#Preview {
+    Navigation2()
+}
